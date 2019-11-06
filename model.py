d8a839324465d3c48c54035f1e55a9857f512eaa
Elliott Huangfu <hfyxin@gmail.com>
2019-11-06 16:30:42 -0500

    Add CNN + RNN implementation

--- model.py
+++ model.py
@@ -21,13 +21,13 @@ def cnn_1d(vocab_size, seq_len):
     https://github.com/keras-team/keras/blob/master/examples/imdb_cnn.py
     """
 
-    embedding_dims = 50
+    embedding_size = 50
     filters = 250
     kernel_size = 3
     hidden_dims = 250
     model = tf.keras.Sequential([
         tf.keras.layers.Embedding(vocab_size,
-                                  embedding_dims,
+                                  embedding_size,
                                   input_length=seq_len), # specify this argument,
                                         # so that model.summary() can be more explicit
         tf.keras.layers.Dropout(0.2),
@@ -58,14 +58,52 @@ def lstm_1layer(vocab_size, seq_len):
     The original keras example is here:
     https://github.com/keras-team/keras/blob/master/examples/imdb_lstm.py
     """
-    embedding_dims = 128
+    embedding_size = 128
     model = tf.keras.Sequential([
         tf.keras.layers.Embedding(vocab_size,
-                                  embedding_dims,
+                                  embedding_size,
                                   input_length=seq_len),
-        tf.keras.layers.LSTM(units=embedding_dims,
+        tf.keras.layers.LSTM(units=embedding_size,
                              dropout=0.2,
                              recurrent_dropout=0.2),
         tf.keras.layers.Dense(1, activation='sigmoid')
     ])
+    return model
+
+
+def cnn_lstm(vocab_size, seq_len):
+    """
+    Return a keras model with 1 layer CNN and then 1 layer LSTM:
+        Input Sequence (length: seq_len)
+        |
+        Word Embedding
+        |
+        1D Convolution
+        |
+        RNN cells (LSTM)
+        |
+        FC to 1 output
+    The original keras example is here:
+    https://github.com/keras-team/keras/blob/master/examples/imdb_cnn_lstm.py
+    """
+    embedding_size = 128
+    kernel_size = 5
+    n_filters = 64
+    pool_size = 4
+    lstm_output_size = 70
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size,
+                                  embedding_size,
+                                  input_length=seq_len),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv1D(n_filters,
+                               kernel_size,
+                               padding='valid',
+                               activation='relu',
+                               strides=1),
+        tf.keras.layers.MaxPooling1D(pool_size),
+        tf.keras.layers.LSTM(lstm_output_size),
+        tf.keras.layers.Dense(1, activation='sigmoid'),
+    ])
+
     return model
--- single_demo.py
+++ single_demo.py
@@ -1,8 +1,20 @@
-# Load imdb dataset
 from tensorflow.keras.datasets import imdb
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from model import cnn_1d, lstm_1layer, cnn_lstm
 
-n_vocab = 5000     # vocabulary size
+# Parameters
+n_vocab = 20000     # vocabulary size
+maxlen = 100        # length of each example (paragraph)
+models = {          # model selection
+    1:cnn_1d,
+    2:lstm_1layer,
+    3:cnn_lstm,
+}
+n = 3              # number of model
+batch_size = 30    # some models are batch-size sensitive
+epochs = 3
+
+# Load imdb dataset
 (x_train, y_train), (x_test, y_test) = imdb.load_data(
     num_words=n_vocab,
     # skip_top=0,
@@ -13,7 +25,6 @@ n_vocab = 5000     # vocabulary size
     # index_from=3,
 )
 
-maxlen = 400    # length of each example
 x_train = pad_sequences(x_train, maxlen=maxlen)
 x_test = pad_sequences(x_test, maxlen=maxlen)
 
@@ -22,24 +33,16 @@ print('x_test shape:', x_test.shape)
 
 
 # Model set-up
-from model import cnn_1d, lstm_1layer
-models = {
-    1:cnn_1d,
-    2:lstm_1layer,
-}
-n = 1     # model selection
 model = models[n](n_vocab, maxlen)
 model.summary()
 
-# Training
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-batch_size = 512
-epochs = 3
+# Training
 model.fit(x_train, y_train,
           batch_size=batch_size,
-          verbose=2,
+          verbose=1,
           epochs=epochs,
           validation_data=(x_test, y_test))
